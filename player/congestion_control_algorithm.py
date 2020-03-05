45a34683a45a564e0d2418c4ed711141556b365b
azson <240326315@qq.com>
2020-03-05 17:22:55 +0800

    add reno cc from gyr branch

--- player/congestion_control_algorithm.py
+++ player/congestion_control_algorithm.py
@@ -6,9 +6,11 @@ class Solution(object):
     def __init__(self):
         self._input_list = []
         self.call_nums = 0
-        self.cwnd = 10
+        self.cwnd = 1
         self.send_rate = 700
-
+        self.ssthresh = 16
+        self.curr_state = "slow_start"
+        self.states = ["slow_start", "congestion_avoidance", "fast_recovery"]
 
     def make_decision(self):
         self.call_nums += 1
@@ -22,13 +24,38 @@ class Solution(object):
 
 
     def cc_trigger(self, data):
-        pass
+        print(self.cwnd)
+        packet_type = data["packet_type"]
+
+        while True:
+            if self.curr_state == self.states[0]:
+                if packet_type != 'D':
+                    self.cwnd *= 2
+                    if self.cwnd >= self.ssthresh:
+                        self.curr_state = self.states[1]
+                break
+
+            if self.curr_state == self.states[1]:
+                if packet_type != 'D':
+                    self.cwnd += 1
+                else:
+                    self.curr_state = self.states[2]
+                break
+
+            if self.curr_state == self.states[2]:
+                self.ssthresh = self.cwnd // 2 + 3
+                self.cwnd = self.ssthresh
+                self.curr_state = self.states[1]
+                break
+
+
+
 
 
     def append_input(self, data):
         self._input_list.append(data)
 
-        if data["packet_type"] != PACKET_TYPE_TEMP:
+        if data["packet_type"] not in PACKET_TYPE_TEMP:
             self.cc_trigger(data)
             return {
                 "cwnd" : self.cwnd,
