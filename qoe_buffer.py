38eb468f0178c8929f6a51d32b7147f80eeee6f0
bladelei <3066835069@qq.com>
2020-04-04 20:46:30 +0800

    updated the qoe based on buffer changing

---
+++ qoe_buffer.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from objects.pcc_emulator import PccEmulator
+import os, sys, inspect
+from config.constant import *
+import numpy as np
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+from player.aitrans_solution import Solution as s1
+from player.aitrans_solution2 import Solution as s2
+
+def cal_qoe(x):
+    block_data = []
+    urgency = []
+    priorities = []
+    qoe = 0
+    with open("output/block.log", "r") as f:
+        for line in f.readlines():
+            block_data.append(json.loads(line.replace("'", '"')))
+    for block in block_data:
+        priority = float((int(block['Priority']) + 1) / 3)
+        priorities.append(priority)
+        if block['Miss_ddl'] == 0:
+            urgency.append(1)
+        else:
+            urgency.append(0)
+    for i in range(len(urgency)):
+        qoe += x * priorities[i] + (1 - x) * urgency[i]
+    return qoe
+
+def cal_distance(block_file, trace_file, x):
+    emulator1 = PccEmulator(
+        block_file=block_file,
+        trace_file=trace_file,
+        queue_range=(MIN_QUEUE, MAX_QUEUE),
+        solution=s1()
+    )
+    emulator1.run_for_dur(float("inf"))
+    reno_qoe = cal_qoe(x)
+
+    emulator2 = PccEmulator(
+        block_file=block_file,
+        trace_file=trace_file,
+        queue_range=(MIN_QUEUE, MAX_QUEUE),
+        solution=s2()
+    )
+    emulator2.run_for_dur(float("inf"))
+    bbr_qoe = cal_qoe(x)
+    return [reno_qoe, bbr_qoe]
+
+def plt_qoe(reno_arr, bbr_arr):
+    x = np.linspace(10, 60, 50)
+    fig, ax = plt.subplots()
+    ax.plot(x,reno_arr, color="blue", label="reno_qoe")
+    ax.plot(x, bbr_arr, color="red", label="bbr_qoe")
+    ax.set_xlabel("queue_size")
+    ax.set_ylabel("qoe")
+    ax.legend()
+    plt.savefig("qoemodel/qoes_buffer.png")
+
+
+if __name__ == '__main__':
+
+    block_file = "config/block.txt"
+    trace_file = "config/trace.txt"
+    log_file = "output/pcc_emulator.log"
+    log_packet_file = "output/packet_log/packet-0.log"
+    x = 0.09
+    reno_arr = []
+    bbr_arr = []
+    for j in range(10, 60):
+        MAX_QUEUE = MIN_QUEUE = j
+        qoe_difference = cal_distance(block_file, trace_file, x)
+        reno_arr.append(qoe_difference[0])
+        bbr_arr.append(qoe_difference[1])
+
+    plt_qoe(reno_arr, bbr_arr)
+
+    with open("qoemodel/qoes_buffer.log","w+") as f:
+        strs = ["buffer: MAX_QUEUE = 10, MIN_QUEUE = 60\n",
+
+                "qoe = 0.09 * priority + 0.91 * deadline\n",
+                str(reno_arr) + "\n",
+                str(bbr_arr)]
+        f.writelines(strs)
+
+
+
+
+
+
+
