55dd5a6c50829a6f61d7afb32fd6acf16bbc7a33
azson <240326315@qq.com>
2020-06-03 22:15:53 +0800

    delete cc.py in player

--- player/aitrans_3.py
+++ player/aitrans_3.py
@@ -1,4 +1,3 @@
-from player.congestion_control_algorithm import Solution as CcSolution
 from player.packet_selection import Solution as PacketSelection
 from player.examples.simple_bbr import BBR
 from player.examples.match_trace_rate import MTR
--- player/aitrans_solution2.py
+++ player/aitrans_solution2.py
@@ -1,4 +1,3 @@
-from player.congestion_control_algorithm import Solution as CcSolution
 from player.packet_selection import Solution as PacketSelection
 from player.examples.simple_bbr import BBR
 
